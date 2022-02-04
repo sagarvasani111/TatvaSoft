18b176007a8c92cea15e2cb598a5b8efaef9d0af
sagarvasani111 <89208574+sagarvasani111@users.noreply.github.com>
2022-02-04 23:00:42 +0530

    Add files via upload

---
+++ helperland/viewmodel/RegisterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Helperland.viewmodel
+{
+    public class RegisterViewModel
+    {
+        internal int UserId;
+
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+      
+        [Required]
+        public string Mobile { get; set; }
+        [Required]
+        [Compare("ConfirmPassword", ErrorMessage="Passwords must match")]
+        public string Password { get; set; }
+        [Required]
+        public string ConfirmPassword { get; set; }
+    }
+}
