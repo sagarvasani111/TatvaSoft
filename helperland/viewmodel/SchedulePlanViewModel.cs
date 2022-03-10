c276fabba3d69ef6d1ff1d4687ef7d9839b37d94
sagarvasani111 <89208574+sagarvasani111@users.noreply.github.com>
2022-03-10 10:30:06 +0530

    Add files via upload

---
+++ helperland/viewmodel/SchedulePlanViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Helperland.viewmodel
+{
+    public class SchedulePlanViewModel
+    {
+
+        [DataType(DataType.Date)]
+        public string ServiceStartDate { get; set; }
+
+        public string StartTime { get; set; }
+
+        public string StayTime { get; set; }
+
+        public float TotalTime { get; set; }
+
+        public float TotalCost { get; set; }
+
+        public float EffctiveCost { get; set; }
+
+        public List<bool> Extra { get; set; }
+
+        public string comment { get; set; }
+        public bool IsPet { get; set; }
+    }
+}
