ede45ef125dc875ca5b77ea295ffc049093e18fa
sagarvasani111 <89208574+sagarvasani111@users.noreply.github.com>
2022-03-10 10:29:33 +0530

    Add files via upload

---
+++ helperland/viewmodel/PostalCodeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HelperLand.ViewModels
+{
+    public class PostalCodeViewModel
+    {
+        [Required(ErrorMessage = "Postal Code is required")]
+        public string PostalCode { get; set; }
+    }
+}
