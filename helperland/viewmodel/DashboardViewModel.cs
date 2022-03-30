7c47fa4c8ea081053573ea6cc8890ca1d8c7d3fb
sagarvasani111 <89208574+sagarvasani111@users.noreply.github.com>
2022-03-30 09:57:15 +0530

    Add files via upload

---
+++ helperland/viewmodel/DashboardViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Helperland.Models;
+using Helperland.viewmodel;
+
+namespace HelperLand.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public List<ServiceRequest> dashboardData { get; set; }
+
+        public List<ServiceRequest> serviceHistory { get; set; }
+        public ServiceRescheduleViewModel serviceReschedule { get; set; }
+
+        public List<User> userData { get; set; }
+
+        public User uData { get; set; }
+
+        public List<UserAddress> userAddressData { get; set; }
+
+        public UserAddress userAddress1 { get; set; }
+
+        public int EditPerticularAddress { get; set; }
+
+        public int CancelServiceId { get; set; }
+
+        public ChangePasswordViewModel changePassViewModel { get; set; }
+        public UserAddress add { get; set; }
+    }
+}
---
+++ helperland/viewmodel/ServiceProviderDetailsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Helperland.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Helperland.viewmodel
+{
+    public class ServiceProviderDetailsViewModel
+    {
+        public User User { get; set; }
+
+        public UserAddress Address { get; set; }
+ 
+        public UserAddress AddID { get; set; }
+        public string Addressline1 { get; set; }
+        public string Addressline2 { get; set; }
+        public string City { get; set; }
+        public string Zipcode { get; set; }
+    }
+}
